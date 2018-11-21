3a3011a86df8bc80751010601170ced3ceeba262
erivan <erivan.nas@gmail.com>
2018-11-21 21:35:12 -0200

    Arquitetura 1.0

--- src/model/Departamento.java
+++ src/model/Departamento.java
@@ -13,18 +13,11 @@ public class Departamento {
     @Id @GeneratedValue
     private int id;
 
-    private String titulo;
-
-    private String descricao;
-
-    private int preco;
-
     //Essa anotação indica que o atributo não é persistente
     @Transient
     private int runtimeId;
 
-
-    public Filme() {}
+    public Departamento() {}
 
     //getters e setters dos atributos
 
@@ -36,28 +29,12 @@ public class Departamento {
         this.id = id;
     }
 
-    public String getTitulo() {
-        return this.titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
     public String getDescricao() {
         return this.descricao;
     }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
-    }
-
-    public int getPreco() {
-        return this.preco;
-    }
-
-    public void setPreco(int preco) {
-        this.preco = preco;
     }
 
     public int getRuntimeId() {
