0487dc63fff94283b76a9c21eee3fd371cf29a39
augusto-moura <augusto.borgesm@gmail.com>
2018-11-25 23:50:07 -0200

    Implementação casca do projeto

---
+++ src/main/java/br/ufg/inf/es/integracao/inventario/view/Aplicacao.java
@@ -0,0 +1,21 @@
+package br.ufg.inf.es.integracao.inventario.view;
+
+/**
+ * Representa o ponto de início da execução de uma aplicação
+ *
+ * @param <P> Estrutura que carrega os parametros necessários para a aplicação.
+ *           Caso não haja nenhum parametro, declarar como {@link java.lang.Void}
+ */
+public interface Aplicacao<P> {
+
+    /**
+     * Executa a aplicação. Geralmente deverá ser executada apenas uma vez, mas
+     * não se faz obrigatório por contrato, segurança contra concorrência
+     * também não garantida.
+     *
+     * @param parametros Parametros usados na configuração da aplicação, a
+     *                   implementação deve ser segura contra argumentos nulos.
+     */
+    void run(final P parametros);
+
+}
---
+++ src/main/java/br/ufg/inf/es/integracao/inventario/view/cli/AplicacaoCli.java
@@ -0,0 +1,15 @@
+package br.ufg.inf.es.integracao.inventario.view.cli;
+
+import br.ufg.inf.es.integracao.inventario.view.Aplicacao;
+
+public class AplicacaoCli implements Aplicacao<String[]> {
+
+    public AplicacaoCli() {
+    }
+
+    @Override
+    public void run(final String[] parametros) {
+        // TODO: Implementar aplicação
+    }
+
+}
