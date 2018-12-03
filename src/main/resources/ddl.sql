fa1c4056b26813dc12ac249da07ec1992c1906f9
Márcio Flores <marciogabriel07@gmail.com>
2018-12-03 16:32:44 -0200

    Alteração do DDL
    Definição de atributos

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -6,42 +6,35 @@ CREATE TABLE IF NOT EXISTS unidade (
 	uf       VARCHAR(2),
 	cep      TEXT
 );
-
-CREATE TABLE IF NOT EXISTS predio (
+ CREATE TABLE IF NOT EXISTS predio (
 	id         BIGSERIAL PRIMARY KEY,
 	nome       TEXT,
 	unidade_id BIGINT REFERENCES unidade
 );
-
-CREATE TABLE IF NOT EXISTS departamento (
+ CREATE TABLE IF NOT EXISTS departamento (
 	id   BIGSERIAL PRIMARY KEY,
-	nome TEXT,
-	chefe BIGINT REFERENCES usuario
+	nome TEXT
 );
-
-CREATE TABLE IF NOT EXISTS sala (
+ CREATE TABLE IF NOT EXISTS sala (
 	id              BIGSERIAL PRIMARY KEY,
 	nome            TEXT,
 	codigo          TEXT,
 	predio_id       BIGINT REFERENCES predio,
 	departamento_id BIGINT REFERENCES departamento
 );
-
-CREATE TABLE IF NOT EXISTS usuario (
+ CREATE TABLE IF NOT EXISTS usuario (
 	id      BIGSERIAL PRIMARY KEY,
 	nome    TEXT,
 	email   TEXT,
 	senha   TEXT,
 	sala_id BIGINT REFERENCES sala
 );
-
-CREATE TABLE IF NOT EXISTS usuario_papel (
+ CREATE TABLE IF NOT EXISTS usuario_papel (
 	usuario_id BIGINT REFERENCES usuario,
 	papel      VARCHAR(50),
 	PRIMARY KEY (usuario_id, papel)
 );
-
-CREATE TABLE IF NOT EXISTS bem_patrimonial (
+ CREATE TABLE IF NOT EXISTS bem_patrimonial (
 	id                   BIGSERIAL PRIMARY KEY,
 	data_aquisicao       DATE,
 	numero_tombamento    TEXT,
@@ -55,15 +48,13 @@ CREATE TABLE IF NOT EXISTS bem_patrimonial (
 	incorporado          BOOLEAN,
 	localizacao_atual_id BIGINT REFERENCES sala
 );
-
-CREATE TABLE IF NOT EXISTS baixa (
+ CREATE TABLE IF NOT EXISTS baixa (
 	id                 BIGSERIAL PRIMARY KEY,
 	"data"             DATE,
 	comentario         TEXT,
 	bem_patrimonial_id BIGINT REFERENCES bem_patrimonial
 );
-
-CREATE TABLE IF NOT EXISTS movimentacao (
+ CREATE TABLE IF NOT EXISTS movimentacao (
 	id                     BIGSERIAL PRIMARY KEY,
 	origem_id              BIGINT REFERENCES sala,
 	destion_id             BIGINT REFERENCES sala,
@@ -71,20 +62,22 @@ CREATE TABLE IF NOT EXISTS movimentacao (
 	autorizador_entrada_id BIGINT REFERENCES usuario,
 	situacao               VARCHAR(50)
 );
-
-CREATE TABLE IF NOT EXISTS cancelamento (
+ CREATE TABLE IF NOT EXISTS cancelamento (
 	id              BIGSERIAL PRIMARY KEY,
 	"data"          DATE,
 	motivo          TEXT,
 	movimentacao_id BIGINT REFERENCES movimentacao
 );
-
-CREATE TABLE IF NOT EXISTS ordem_servico (
+ CREATE TABLE IF NOT EXISTS ordem_servico (
 	id               BIGSERIAL PRIMARY KEY,
 	data_criacao     DATE,
 	situacao         VARCHAR(50),
 	inicio_servico   DATE,
 	fim_servico      DATE,
 	responsavel_id   BIGINT REFERENCES usuario,
-	bem_patrimonial_id BIGINT REFERENCES bem_patrimonial
+	ordem_servico_id BIGINT REFERENCES ordem_servico
 );
+ALTER TABLE departamento
+ADD IF NOT EXISTS chefe BIGINT REFERENCES usuario;
+ALTER TABLE sala
+ADD IF NOT EXISTS bem_patrimonial_id BIGINT REFERENCES bem_patrimonial;
