2784a5575cd636a655597a3b3d00dbf5cdf64579
Paulo Junio Sales Rodrigues <paulo.rodrigues@globaltec.com.br>
2018-12-04 14:04:56 -0200

    CRUDs

---
+++ src/main/java/br/ufg/inf/es/integracao/inventario/repositorio/BaixaBemRepository.java
@@ -0,0 +1,26 @@
+package br.ufg.inf.es.integracao.inventario.repositorio;
+
+import br.ufg.inf.es.integracao.inventario.dominio.entidade.Predio;
+import org.apache.commons.io.IOUtils;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class BaixaBem {
+  private final Supplier<Connection> connectionProvider;
+
+  private static final Charset DDL_FILE_CHARSET = Charset.forName("UTF-8");
+  private static final String INSERT_SQL = "/baixabem/insert_baixabem.sql";
+  private static final String SELECT_SQL = "/baixabem/select_baixabem.sql";
+  private static final String DELETE_SQL = "/baixabem/delete_baixabem.sql";
+
+  @Inject
+  public BaixaBemRepository(final Supplier<Connection> connectionProvider) {
+    this.connectionProvider = connectionProvider;
+  }
+}
