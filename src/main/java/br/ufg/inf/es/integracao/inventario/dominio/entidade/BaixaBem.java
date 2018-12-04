6ccb4521445c8ae23aeb0432cbe7d9b91d4f8e7d
Márcio Flores <marciogabriel07@gmail.com>
2018-12-04 17:40:49 -0200

    Revert "Adicionando CRUDs"
    This reverts commit 28ea25829207d040834ddf36229f64c2db9cff22.

--- src/main/java/br/ufg/inf/es/integracao/inventario/dominio/entidade/BaixaBem.java
+++ src/main/java/br/ufg/inf/es/integracao/inventario/dominio/entidade/BaixaBem.java
@@ -6,31 +6,7 @@ import java.util.Date;
  * Created by Alunoinf_2 on 03/12/2018.
  */
 public class BaixaBem {
-  private long id;
   private Date data;
+
   private String comentario;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Date getData() {
-    return data;
-  }
-
-  public void setData(Date data) {
-    this.data = data;
-  }
-
-  public String getComentario() {
-    return comentario;
-  }
-
-  public void setComentario(String comentario) {
-    this.comentario = comentario;
-  }
 }
--- src/main/java/br/ufg/inf/es/integracao/inventario/dominio/entidade/BemPatrimonial.java
+++ src/main/java/br/ufg/inf/es/integracao/inventario/dominio/entidade/BemPatrimonial.java
@@ -8,104 +8,24 @@ import java.util.Date;
  */
 public class BemPatrimonial {
 
-  private long id;
   private Date dataAquisicao;
+
   private String numeroTombamento;
+
   private String numeroNotaFiscal;
+
   private long vidaUtil;
+
   private String especificacao;;
+
   private Date dataGarantia;
+
   private String marca;
+
   private int grupo;
-  private BigDecimal valorCompra
+
+  private BigDecimal valorCompra;
+
   private boolean incorporado;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Date getDataAquisicao() {
-    return dataAquisicao;
-  }
-
-  public void setDataAquisicao(Date dataAquisicao) {
-    this.dataAquisicao = dataAquisicao;
-  }
-
-  public String getNumeroTombamento() {
-    return numeroTombamento;
-  }
-
-  public void setNumeroTombamento(String numeroTombamento) {
-    this.numeroTombamento = numeroTombamento;
-  }
-
-  public String getNumeroNotaFiscal() {
-    return numeroNotaFiscal;
-  }
-
-  public void setNumeroNotaFiscal(String numeroNotaFiscal) {
-    this.numeroNotaFiscal = numeroNotaFiscal;
-  }
-
-  public long getVidaUtil() {
-    return vidaUtil;
-  }
-
-  public void setVidaUtil(long vidaUtil) {
-    this.vidaUtil = vidaUtil;
-  }
-
-  public String getEspecificacao() {
-    return especificacao;
-  }
-
-  public void setEspecificacao(String especificacao) {
-    this.especificacao = especificacao;
-  }
-
-  public Date getDataGarantia() {
-    return dataGarantia;
-  }
-
-  public void setDataGarantia(Date dataGarantia) {
-    this.dataGarantia = dataGarantia;
-  }
-
-  public String getMarca() {
-    return marca;
-  }
-
-  public void setMarca(String marca) {
-    this.marca = marca;
-  }
-
-  public int getGrupo() {
-    return grupo;
-  }
-
-  public void setGrupo(int grupo) {
-    this.grupo = grupo;
-  }
-
-  public BigDecimal getValorCompra() {
-    return valorCompra;
-  }
-
-  public void setValorCompra(BigDecimal valorCompra) {
-    this.valorCompra = valorCompra;
-  }
-
-  public boolean isIncorporado() {
-    return incorporado;
-  }
-
-  public void setIncorporado(boolean incorporado) {
-    this.incorporado = incorporado;
-  }
 }
 
