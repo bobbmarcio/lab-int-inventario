88d889ff7adc451e26d13120661616bcf2d3bf2b
Márcio Flores <marciogabriel07@gmail.com>
2018-12-04 19:44:53 -0200

    Correção do menu

--- src/main/java/br/ufg/inf/es/integracao/inventario/view/cli/GerenciarDepartamentosView.java
+++ src/main/java/br/ufg/inf/es/integracao/inventario/view/cli/GerenciarDepartamentosView.java
@@ -98,7 +98,7 @@ public class GerenciarDepartamentosView {
     out.println("-----------");
     out.println("Lista de departamentos");
     out.println();
-    out.println("ID\tNome\tUnidade");
+    out.println("ID\tNome\tchefe");
     out.println("----------------------------------------------");
 
     final List<Departamento> departamentos = gerenciadorDepartamentos.listarTodos();
